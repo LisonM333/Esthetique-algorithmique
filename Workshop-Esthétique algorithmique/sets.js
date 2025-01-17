5c98512b82861b506e41c4135272d176800d08fa
LisonM333 <li.marvin@laposte.net>
2025-01-17 14:35:49 +0100

    Meilleur présentation

--- Workshop-Esthétique algorithmique/sets.js
+++ Workshop-Esthétique algorithmique/sets.js
@@ -4,7 +4,7 @@ let Sets_sugary= [[6, 8, 3, 5], [1, 2, 7, 9], [6, 3, 4, 2], [1, 4, 2, 3], [8, 5,
 let Sets_acid=[[9, 8, 1, 0], [8, 6, 9, 1], [0, 5, 3, 7], [6, 4, 8, 2], [4, 2, 7, 6], [2, 5, 6, 7], [9, 1, 6, 0], [0, 4, 3, 5], [8, 1, 0, 5], [0, 8, 7, 6]]
 
 function setup(){
-  createCanvas(Sets_sugary.length*200+10+Sets_acid.length*200, 400+260);
+  createCanvas(Sets_sugary.length*200+50+Sets_acid.length*200, 400+260);
   background(255);
 }
 
@@ -14,7 +14,8 @@ function draw() {
   rotate(-HALF_PI);
   textFont('Verdana',16);
   fill(10);
-  stroke(50);
+  stroke(58, 101, 99);
+  strokeWeight(1);
   text('Sugary', 0, 0);
   pop();
   for (let j = 0; j< Sets_sugary.length; j++){
@@ -22,37 +23,49 @@ function draw() {
       let couleur = Couleur_sugary[Sets_sugary[j][i]];
       fill(couleur);
       noStroke();
-      rect(200*j+20+i*40,20,40,160);
+      rect(200*j+40+i*40,20,40,160);
       fill(couleur);
       noStroke();
-      rect(200*j+20+i*20,220+30*i,160-40*i,160-40*i);
+      rect(200*j+40+i*20,220+30*i,160-40*i,160-40*i);
       fill(couleur);
       noStroke();
-      rect (200*j+30, 420+60*i,40,40);
+      rect (200*j+50, 420+60*i,40,40);
       textFont('Verdana',16);
       fill(58, 101, 99);
       noStroke();
-      text(couleur,200*j+80,445+60*i);
+      text(couleur,200*j+100,445+60*i);
     }
     noFill();
     stroke(58, 101, 99);
     strokeWeight(1);
-    rect(200*j+20,410,160,240);
+    rect(200*j+40,410,160,240);
+    stroke(58, 101, 99);
+    strokeWeight(2);
+    line(200*j+220, 20, 200*j+220, 650);
+    // noFill();
+    // stroke(58, 101, 99);
+    // strokeWeight(1);
+    // rect(200*j+20,10,190,640);
     textFont('Verdana',10);
     fill(0);
     noStroke();
-    text(j+1+'.',200*j+5,180);
-    textFont('Verdana',10);
-    fill(0);
-    noStroke();
-    text(j+1+'.',200*j+5,380);
+    text(j+1+'.',200*j+25,180);
+    // textFont('Verdana',10);
+    // fill(0);
+    // noStroke();
+    // text(j+1+'.',200*j+25,380);
   }
+  stroke(224, 238, 232);
+  strokeWeight(8);
+  line(200*Sets_sugary.length+20,0, 200*Sets_sugary.length+20,660);
+
   push();
-  translate(Sets_sugary.length*200+25, 110);
+  translate(Sets_sugary.length*200+45, 110);
   rotate(-HALF_PI);
   textFont('Verdana',16);
   fill(10);
-  stroke(50);
+  stroke(58, 101, 99);
+  strokeWeight(1);
   text('Acid', 0, 0);
   pop();
   for (let j = 0; j< Sets_acid.length; j++){
@@ -60,41 +73,37 @@ function draw() {
       let couleur = Couleur_acid[Sets_acid[j][i]];
       fill(couleur);
       noStroke();
-      rect(Sets_sugary.length*200+10+200*j+20+i*40,20,40,160);
+      rect(Sets_sugary.length*200+50+200*j+20+i*40,20,40,160);
       fill(couleur);
       noStroke();
-      rect(Sets_sugary.length*200+10+200*j+20+i*20,220+30*i,160-40*i,160-40*i);
+      rect(Sets_sugary.length*200+50+200*j+20+i*20,220+30*i,160-40*i,160-40*i);
       fill(couleur);
       noStroke();
-      rect (Sets_sugary.length*200+10+200*j+30, 420+60*i,40,40);
+      rect (Sets_sugary.length*200+50+200*j+30, 420+60*i,40,40);
       textFont('Verdana',16);
       fill(58, 101, 99);
       noStroke();
-      text(couleur,Sets_sugary.length*200+10+200*j+80,445+60*i);
+      text(couleur,Sets_sugary.length*200+50+200*j+80,445+60*i);
 
+    }
+    if (j!=Sets_acid.length-1){
+      stroke(58, 101, 99);
+      strokeWeight(2);
+      line(Sets_sugary.length*200+30+200*j+220, 20, Sets_sugary.length*200+30+200*j+220, 650);
     }
     noFill();
     stroke(58, 101, 99);
     strokeWeight(1);
-    rect(Sets_sugary.length*200+10+200*j+20,410,160,240);
+    rect(Sets_sugary.length*200+50+200*j+20,410,160,240);
     textFont('Verdana',10);
     fill(0);
     noStroke();
-    text(j+1+'.',Sets_sugary.length*200+10+200*j+5,180);
-    textFont('Verdana',10);
-    fill(0);
-    noStroke();
-    text(j+1+'.',Sets_sugary.length*200+10+200*j+5,380);
+    text(j+1+'.',Sets_sugary.length*200+50+200*j+5,180);
+    // textFont('Verdana',10);
+    // fill(0);
+    // noStroke();
+    // text(j+1+'.',Sets_sugary.length*200+50+200*j+5,380);
     
   }
   noLoop();
 }
-
-  
-// for (let i=0;i<4;i++){
-//   fill(Couleur2[i+3]);
-//   noStroke();
-//   rect (20, 20+60*i, 40,40);
-//   textFont('Verdana',16);
-//   text(Couleur2[i],80,45+60*i);
-// }
